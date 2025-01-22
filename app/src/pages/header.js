bbf4a889746ca7cc81e4dda03309a0a21e85d854
husseinalaa21 <hussein.scs@hotmail.com>
2025-01-21 21:22:27 -0500

    F

--- app/src/pages/header.js
+++ app/src/pages/header.js
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import '../style/main.css';
 import logo from '../logos/logo.png';
 import caret_down from '../svg/caret-down-solid.svg'
+import caret_up from '../svg/caret-up-solid.svg'; // Make sure you have this image
 import fire_solid from '../svg/fire-solid.svg'
 import moon_regular from '../svg/moon-regular.svg'
 import moon_solid from '../svg/moon-solid.svg'
@@ -13,6 +14,13 @@ function Header(props) {
     const navigate = useNavigate();
     const [showOptions, setShowOptions] = useState(false);
     const [isNight, setIsNight] = useState(true)
+
+    const [showList, setShowList] = useState(false); // State to control the visibility of the "More" list
+
+    const toggleList = () => {
+        setShowList(prev => !prev); // Toggle the visibility of the list
+    };
+
     // State to track page type
     const location = useLocation();
     const [pageLocated, setPageLocated] = useState({
@@ -54,55 +62,78 @@ function Header(props) {
     }
 
     return (
-        <header className="main_header" onClick={toggleOptions_advance}>
-            <div className='logo-main'>
-                <img className="logo" src={logo} alt="logo" />
-            </div>
+        <>
+            <header className="main_header" onClick={toggleOptions_advance}>
+                <div className='logo-main'>
+                    <img className="logo" src={logo} alt="logo" />
+                </div>
 
-            {props.login ?
-                <>
-                    {!props.ask && (
-                        <div className='picure_header' onClick={toggleOptions}>
-                            <img src={props.pic} alt="Profile" style={{ width: '100%', height: '100%' }} />
-                        </div>
-                    )}
-                </>
-                : props.ask ?
-                    <div className='header_intro'>
-                        <div className='header-ask'>
-                            <div
-                                className='header-login'
-                                onClick={() => navigate('/auth')}
-                            >
-                                Login
+                {props.login ?
+                    <>
+                        {!props.ask && (
+                            <div className='picure_header' onClick={toggleOptions}>
+                                <img src={props.pic} alt="Profile" style={{ width: '100%', height: '100%' }} />
                             </div>
-                            <div
-                                className='header-signup'
-                                onClick={() => navigate('/auth')}
-                            >
-                                Sign Up
+                        )}
+                    </>
+                    : props.ask ?
+                        <div className='header_intro'>
+                            <div className='header-ask'>
+                                <div
+                                    className='header-login'
+                                    onClick={() => navigate('/auth')}
+                                >
+                                    Login
+                                </div>
+                                <div
+                                    className='header-signup'
+                                    onClick={() => navigate('/auth')}
+                                >
+                                    Sign Up
+                                </div>
                             </div>
-                        </div>
-                        <div className='panel'>
-                            <div className='panel_web'>
-                                {/* add here if it was scroll down make the logo visable */}
-                                {/* Make this if the page was diffrent than the home make home button visable */}
-                                {pageLocated.isHome? <div onClick={() => navigate('/auth')}> <mark>Join</mark> today for free </div> : <div onClick={ () => navigate('/')}> Main Page </div>}
-                            </div>
-                            <div className={pageLocated.isAbout ? 'header_selected' : ''} onClick={() => navigate('/about')} > About US </div>
-                            <div className={pageLocated.isSupport ? 'header_selected' : ''} onClick={() => navigate('/support')}> Support </div>
-                            <div className='panel_more'> More <img src={caret_down} width='10px'></img>  </div>
-                        </div>
-                    </div> : null}
-            {/**isNight ? <div className='night_on'> <img src={moon_solid} width='20px'></img></div> : <div className='night_off'></div>**/}
-            {showOptions && (
-                <div className='dropdown_menu'>
-                    <div onClick={() => navigate('/profile')}>Profile</div>
-                    <div onClick={() => navigate('/settings')}>Settings</div>
-                    <div onClick={logout}>Logout</div>
+                            <div className='panel'>
+                                <div className='panel_web'>
+                                    {/* add here if it was scroll down make the logo visable */}
+                                    {/* Make this if the page was diffrent than the home make home button visable */}
+                                    <div onClick={() => navigate('/auth')}> <mark>Join</mark> today for free </div>
+                                </div>
+                                {!pageLocated.isHome && <div onClick={() => navigate('/')}> Main Page </div>}
+                                <div className={pageLocated.isAbout ? 'header_selected' : ''} onClick={() => navigate('/about')} > About US </div>
+                                <div className={pageLocated.isSupport ? 'header_selected' : ''} onClick={() => navigate('/support')}> Support </div>
+
+                                <div className='panel_more' onClick={toggleList}>
+                                    More <img src={showList ? caret_up : caret_down} width='10px' alt="Toggle" />
+                                </div>                        </div>
+                        </div> : null}
+                {/**isNight ? <div className='night_on'> <img src={moon_solid} width='20px'></img></div> : <div className='night_off'></div>**/}
+                {showOptions && (
+                    <div className='dropdown_menu'>
+                        <div onClick={() => navigate('/profile')}>Profile</div>
+                        <div onClick={() => navigate('/settings')}>Settings</div>
+                        <div onClick={logout}>Logout</div>
+                    </div>
+                )}
+            </header>
+            {showList && (
+                <div className="dropdown_content">
+                    <div className="dropdown_section">
+                        <div onClick={() => navigate('/policy')}>Policy</div>
+                        <div onClick={() => navigate('/contact')}>Contact Us</div>
+                        <div onClick={() => navigate('/support')}>Get Support</div>
+                    </div>
+                    <div className="dropdown_section">
+                        <div onClick={() => navigate('/auth/login')}>Log in</div>
+                        <div onClick={() => navigate('/auth/register')}>Create Account</div>
+                    </div>
+                    <div className="dropdown_section">
+                        <div onClick={() => window.location.href = "https://www.linkedin.com/company/university-io/"} target="_blank" rel="noopener noreferrer">LinkedIn</div>
+                        <div onClick={() => window.location.href = "https://x.com/husseinalaa21/"} target="_blank" rel="noopener noreferrer">Twitter</div>
+                        <div onClick={() => window.location.href = "mailto:universityio.office@gmail.com"} target="_blank" rel="noopener noreferrer">Email</div>
+                    </div>
                 </div>
             )}
-        </header>
+        </>
     );
 }
 
