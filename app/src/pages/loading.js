6bc4a429569a1652725e645c51cfd66757996c5e
husseinalaa21 <hussein.scs@hotmail.com>
2025-02-16 13:04:11 -0500

    D

--- app/src/pages/loading.js
+++ app/src/pages/loading.js
@@ -5,8 +5,26 @@ import logo from '../logos/logo.png';
 function Loading() {
     return (
         <div className='loading_main'>
+            <div>
             <div className='loading_logo'>
                 <img className="_logo" src={logo} alt="logo" />
+            </div>
+
+            <div class="scene">
+                <div class="cube-wrapper">
+                    <div class="cube">
+                        <div class="cube-faces">
+                            <div class="cube-face shadow"></div>
+                            <div class="cube-face bottom"></div>
+                            <div class="cube-face top"></div>
+                            <div class="cube-face left"></div>
+                            <div class="cube-face right"></div>
+                            <div class="cube-face back"></div>
+                            <div class="cube-face front"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             </div>
         </div>
     )
